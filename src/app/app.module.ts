28e344131fa9a38b73a3cc1059482fbc3a6fa6e4
aleks_rodriguez <alex_ps1998@hotmail.com>
2020-10-19 18:53:48 +0200

    func(API de Google). Debido a incompatibilidades, el logueo con Google esta deshabilitado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,23 @@ import { NuevoCorreoComponent } from './nuevo-correo/nuevo-correo.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AvisosComponent } from './avisos/avisos.component';
 import { CorreosRecibidosComponent } from './correos-recibidos/correos-recibidos.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {GoogleApiModule,NgGapiClientConfig, NG_GAPI_CONFIG} from "ng-gapi";
+
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "454423071724-g0e9ita6vqqqnorno5qo6rge45ur5mhr.apps.googleusercontent.com",
+  discoveryDocs: ["https://analyticsreporting.googleapis.com/$discovery/rest?version=v4"],
+  ux_mode: "popup",
+  redirect_uri: "http://localhost:4200/loged",
+  scope: [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.readonly"
+  ].join(" ")
+};
 
 @NgModule({
   declarations: [
@@ -17,13 +34,19 @@ import { CorreosRecibidosComponent } from './correos-recibidos/correos-recibidos
     ListaCorreosComponent,
     NuevoCorreoComponent,
     AvisosComponent,
-    CorreosRecibidosComponent
+    CorreosRecibidosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    GoogleApiModule.forRoot({
+      provide: NG_GAPI_CONFIG,
+      useValue: gapiClientConfig
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
